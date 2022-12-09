937f993dc054192f777e54e37439ca3686a12dc3
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2022-12-09 15:28:54 +0530

    Create obsdemo.js

---
+++ 09DecAss/obsdemo.js
@@ -0,0 +1,21 @@
+import {Observable} from 'rxjs';
+const obs = new Observable(
+    subscriber=>{
+        subscriber.next(5);
+        subscriber.next(10);
+        subscriber.next(90);
+        setTimeout(()=>{
+            subscriber.next(89);
+            subscriber.complete();
+        },2000);
+    });
+    console.log('befor subscriber');
+    obs.subscribe({
+        next(i)
+        {
+            console.log(i)},
+        error(e)
+        {
+            console.log(e)},
+        complete(){console.log('all values received')},
+    });
