20090d2f997be4fea607f536fecbc3d8692b0f8d
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2022-12-12 11:27:49 +0530

    Create personalloan.js

---
+++ 12Dec/personalloan.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('loan-form');
+
+form.addEventListener('submit',function(e){
+
+document.getElementById('results').style.display='none';
+
+document.getElementById('loading').style.display='block';
+setTimeout(calculateResults,2000);
+e.preventDefault();
+});
+
+function calculateResults(e){
+console.log('calculating...');
+
+const ELamount = document.getElementById('amount');
+const ELinterest = document.getElementById('interest');
+const ELyears = document.getElementById('years');
+const ELMonthly_payment = document.getElementById('monthly-payment');
+const ELtotal_payment = document.getElementById('total-payment');
+const ELtotal_interest = document.getElementById('total-interest');
+const principal = parseFloat(ELamount.value);
+const calculatedInterest = parseFloat(ELinterest.value) /100 /12;
+const calculatedPayment = parseFloat(ELyears.value )*12;
+
+const x = Math.pow(1 + calculatedInterest, calculatedPayment);
+const monthly = (principal * x * calculatedInterest) / (x - 1);
+console.log(monthly);
+if(isFinite(monthly)){
+ELMonthly_payment.value = monthly.toFixed(2);
+ELtotal_payment.value = (monthly*calculatedPayment).toFixed(2);
+ELtotal_interest.value = ((monthly*calculatedPayment)-principal).toFixed(2);
+
+document.getElementById('results').style.display='block';
+
+document.getElementById('loading').style.display='none';
+}else{
+console.log("Plase check your numbers");
+
+showError('Plase check your number');
+}
+e.preventDefault();
+}
+function showError(error){
+
+document.getElementById('results').style.display='none';
+
+document.getElementById('loading').style.display='none';
+
+const errorDiv = document.createElement('div');
+
+const ELcard = document.querySelector ('.card');
+const ELheading = document.querySelector('.heading');
+
+errorDiv.className = 'alert alert-danger';
+
+errorDiv.appendChild(document.createTextNode(error));
+
+ELcard.insertBefore(errorDiv, ELheading);
+
+setTimeout(clearError, 3000);
+}
+function clearError(){
+document.querySelector('.alert').remove();
+}
