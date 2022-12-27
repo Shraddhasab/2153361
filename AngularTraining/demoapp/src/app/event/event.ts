f2f1c162b6221b209e1007bd2df262c5f04c73a1
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2022-12-27 11:59:10 +0530

    Create event.ts

---
+++ AngularTraining/demoapp/src/app/event/event.ts
@@ -0,0 +1,26 @@
+export interface IEvent{    
+    id:string;
+    name:string;
+    date:string;
+    time:string;
+    price:number;
+    imageUrl:string;
+   location:{
+    address:string;
+    city:string;
+    country:string;
+}
+
+   session:{
+    id:string;
+    name:string;                          
+    presenter:string;
+    duration:string;
+    level:string;
+    voters:string[];
+}[];
+
+    
+
+    
+}
